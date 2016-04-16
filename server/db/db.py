8b0d3545c03a317592a6c5631914a709c5837182
Hiraku Ohno <almichest.free@gmail.com>
2016-04-17 04:00:58 +0900

    とりあえずコミット

--- server/db/db.py
+++ server/db/db.py
@@ -1,20 +1,33 @@
 __author__ = 'hira'
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Float, String
+from sqlalchemy import create_engine, MetaData, Table, Column, DECIMAL, String
+from attention_item import AttantionItem
 
-metadata = MetaData()
-attention_items = Table(
-    'attention_items', metadata,
+__metadata = MetaData()
+__attention_items = Table(
+    'attention_items', __metadata,
     Column('identifier', String, primary_key=True),
     Column('place_name', String),
     Column('attention_body', String),
-    Column('latitude', Float),
-    Column('longitude', Float),
+    Column('latitude', DECIMAL),
+    Column('longitude', DECIMAL),
 )
 
-engine = create_engine('sqlite:///db.sqlite3', echo=True)
-metadata.bind = engine
-metadata.create_all()
+__engine = create_engine('sqlite:///db.sqlite3', echo=True)
+__metadata.bind = __engine
+__metadata.create_all()
+
+print("hoge")
 
 def insert(item):
-    attention_items.insert().execute(identifier='hogehoge', place_name='新宿駅')
+    __attention_items.insert().execute(identifier=item.identifier,
+                                       place_name=item.place_name,
+                                       attention_body=item.attention_body,
+                                       latitude=item.latitude,
+                                       longitude=item.longitude)
+def update(item):
+    __attention_items.update().where(__attention_items.c.identifier == item.identifier).execute(place_name=item.place_name,
+                                                                                                attention_body=item.attention_body,
+                                                                                                latitude=item.latitude,
+                                                                                                longitude=item.longitude)
+
