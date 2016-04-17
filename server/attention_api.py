eba38ba0e6f1342fcd8c7e2385cc050c80a20ab8
Hiraku Ohno <almichest.free@gmail.com>
2016-04-17 22:23:03 +0900

    Enable receiving HTTP POST request.

--- server/attention_api.py
+++ server/attention_api.py
@@ -9,6 +9,23 @@ class AttentionAPI(object):
         }
         resp.body = json.dumps(msg)
 
+    def on_post(self, req, resp):
+        body = b''
+
+        while True:
+
+            chunk = req.stream.read()
+            if not chunk:
+                break
+
+            body += chunk
+            if req.content_length <= len(body):
+                break
+
+        print('request body = ' + str(body))
+
+        resp.body = ''
+
 app = falcon.API()
 app.add_route("/", AttentionAPI())
 
@@ -16,6 +33,7 @@ def main():
     from wsgiref import simple_server
     httpd = simple_server.make_server("127.0.0.1", 8000, app)
     httpd.serve_forever()
+    print('start server')
 
 if __name__ == '__main__':
     main()
