b95e0e5f9aaf037b59be6a7027591503856aa456
Hiraku Ohno <almichest.free@gmail.com>
2016-04-18 22:53:19 +0900

    Enable returning 400 to bad query.

--- server/attention_api.py
+++ server/attention_api.py
@@ -7,25 +7,52 @@ _database = AttentionDatabase(db_name='db.sqlite3')
 
 class Get(object):
 
+    _OK = '0'
+    _INVALID_QUERY = '1'
+
     def on_get(self, req, resp):
         items = _database.get_items()
 
-        if hasattr(req, 'query_string'):
+        if hasattr(req, 'query_string') and 0 < len(req.query_string):
+            print('query = ' + req.query_string)
+            validation = self.validate_query(req.query_string)
+            if validation[0] == '400':
+                resp.status = falcon.HTTP_400
+                error = '{error_code : ' + validation[1] + '}'
+                resp.body = error
+                return
+
             items = self.filter_with_query(req.query_string, items)
 
         resp.body = json.dumps(items)
+
+    def validate_query(self, query):
+        import urllib.parse
+        queries = urllib.parse.parse_qs(query)
+        if not 'longitude' in queries or not 'latitude' in queries or not 'radius' in queries :
+            return ('400', self._INVALID_QUERY )
+        return ('200', self._OK)
+
 
     def filter_with_query(self, query, items):
         print(query)
         return items
 
 class Post(object):
+
+    _OK = '0'
+    _INVALID_CONTENT_TYPE = '1'
+    _INVALID_CONTENT_LENGTH = '2'
+    _INVALID_CONTENT_BODY = '3'
+    _SAME_IDENTIFIER_ALREADY_EXISTS = '4'
+
     def on_post(self, req, resp):
 
         validation = self.validate_post_request_header(req)
-        if validation == '400':
+        if validation[0] == '400':
             resp.status = falcon.HTTP_400
-            resp.body = ''
+            error = '{error_code : ' + validation[1] + '}'
+            resp.body = error
             return
 
         body = b''
@@ -70,19 +97,19 @@ class Post(object):
 
     def validate_post_request_header(self, req):
         if req.content_type != 'application/json':
-            return '400'
+            return ('400', self._INVALID_CONTENT_TYPE)
         if req.content_length <= 0:
-            return '400'
+            return ('400', self._INVALID_CONTENT_BODY)
 
-        return '200'
+        return ('200', self._OK)
 
     def validate_post_request_body(self, body):
         if not 'identifier' in body:
-            return ('400', '1')
+            return ('400', self._INVALID_CONTENT_BODY)
         if _database.get_items(identifier=body['identifier']):
-            return ('400', '2')
+            return ('400', self._SAME_IDENTIFIER_ALREADY_EXISTS)
 
-        return '200'
+        return ('200', self._OK)
 
 app = falcon.API()
 app.add_route('/api/', Get())
