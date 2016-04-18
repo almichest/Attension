6ae508f6bbe9e9642e52def2032df71275dc25c3
Hiraku Ohno <almichest.free@gmail.com>
2016-04-19 00:04:25 +0900

    Enable adding receiver to datasource.

--- client/Attension/AttentionItemDataSource.swift
+++ client/Attension/AttentionItemDataSource.swift
@@ -14,7 +14,28 @@ class AttentionItemDataSource: NSObject {
     
     static let sharedInstance = AttentionItemDataSource()
     private let realm = try! Realm()
-    
+    private lazy var realmToken: NotificationToken = {
+        return self.realm.addNotificationBlock {[weak self] (notification, realm) in
+            guard self != nil else {return}
+            self?.receivers.forEach { (receiver) in
+                receiver.datasetDidChange(self!)
+            }
+        }
+    }()
+
+    private var receivers: [AttentionItemDataSourceReceiver] = []
+
+    func subscribe(receiver: AttentionItemDataSourceReceiver) {
+        guard !receivers.contains({$0 === receiver}) else {return}
+        receivers.append(receiver)
+    }
+
+    func unsubscribe(receiver: AttentionItemDataSourceReceiver) {
+        if let index = receivers.indexOf({$0 === receiver}) {
+            receivers.removeAtIndex(index)
+        }
+    }
+
     func query(latitude: CLLocationDegrees, longtitude: CLLocationDegrees, radius: Double) -> Task<Float, [AttentionItem], NSError> {
         return Task<Float, [AttentionItem], NSError>{ fulfill, reject in
             let result = Array(self.realm.objects(AttentionItem))
@@ -44,5 +65,9 @@ class AttentionItemDataSource: NSObject {
     private func createIdentifier(item: AttentionItem) -> String {
         return String(item.latitude) + String(item.longtitude)
     }
-
 }
+
+protocol AttentionItemDataSourceReceiver: class {
+    func datasetDidChange(dataSource: AttentionItemDataSource)
+}
+
