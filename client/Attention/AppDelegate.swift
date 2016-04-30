3308c70f19eb3e2b9d6f870fd29104739ace471e
Hiraku Ohno <almichest.free@gmail.com>
2016-04-30 20:51:42 +0900

    Add twitter frameworks.

--- client/Attention/AppDelegate.swift
+++ client/Attention/AppDelegate.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-import Firebase
+import Fabric
+import Crashlytics
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self, Twitter.self])
         return true
     }
 
