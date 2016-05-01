03ee17f117052618d5ca98eab6eb563f4516a7db
Hiraku Ohno <almichest.free@gmail.com>
2016-05-01 14:08:19 +0900

    Swifter to submodule.

--- client/Attention/TwitterClient.swift
+++ client/Attention/TwitterClient.swift
@@ -22,28 +22,12 @@ class TwitterClient: NSObject {
 
     func post(text: String) -> Task<Float, Bool, NSError> {
         return Task<Float, Bool, NSError>(promiseInitClosure: { (fulfill, reject) in
-            let string = "hoge : fuga"
-            print(string)
-            self.swifter.postStatusUpdate(string, success: { status in
+            self.swifter.postStatusUpdate(text, success: { status in
                 fulfill(true)
             }, failure: { error in
-                print(error)
+                debugPrint(error)
                 reject(error)
             })
         })
-    }
-}
-
-
-class Test: NSObject {
-    func test() {
-        let swifter = Swifter(consumerKey: TWITTER_CONSUMER_KEY, consumerSecret: TWITTER_CONSUMER_SECRET, oauthToken: TWITTER_ACCESS_TOKEN, oauthTokenSecret: TWITTER_ACCESS_TOKEN_SECRET)
-        let dictionary = ["hoge" : "fuga"]
-        let string = dictionary.description
-        swifter.postStatusUpdate(string, success: { status in
-        }, failure: { error in
-            print(error)
-        })
-
     }
 }
